1f565628c68d5c5f9a5f85a67892a45cbc880a4a
yung lean <33080132+Michael23B@users.noreply.github.com>
2018-06-06 12:09:50 +1000

    Added assign colour command
    Assigns a random colour or the colour based on rgb values provided.
    
    Also changed userinfo to find the member based on the channel so that it can use the users roleColor to colour the embed. Very nice looking :sweat_smile:

--- bot.js
+++ bot.js
@@ -19,36 +19,69 @@ client.on('message', async message => {
     if (message.channel.type === 'dm') return;
     if (!message.content.startsWith(settings.prefix)) return;
 
-    executeCommand(message);
+    await executeCommand(message);
 });
 
-client.login(settings.token);
+client.login(settings.token).catch(console.error);
 
-function executeCommand(message) {
+async function executeCommand(message) {
     let args = message.content.split(' ');
     let command = args[0];
-    args.splice(1);
+    args = args.splice(1);
 
     switch (command) {
         case `${prefix}hello`:
             message.react('👋');
             break;
         case `${prefix}userinfo`:
-            let user = message.author;
-            let embed = CreateUserInfoEmbed(user);
-
+            let user =  message.mentions.members.first() || message.guild.members.find(x => x.user === message.author);
+            let embed = createUserInfoEmbed(user);
             message.channel.send(embed);
             break;
+        case `${prefix}colour`:
+            message.member.colorRole.setColor(getColour(args))
+                .then(updated => message.reply(`colour set to ${updated.hexColor}`))
+                .catch(console.error);
+            break;
         default:
-            console.log(`${command} is not a recognized command!`);
+            message.reply(`${command} is not a recognized command!`);
     }
 }
 
-function CreateUserInfoEmbed(user) {
+//Discord-related helper functions
+function createUserInfoEmbed(user) {
     return new Discord.RichEmbed()
-        .setAuthor(user.username)
-        .setImage(user.avatarURL)
-        .setColor('BLUE')
-        .addField('Full username: ', `${user.username}#${user.discriminator}`)
-        .addField('Joined Discord: ', user.createdAt);
+        .setTitle(`User info - ${user.user.username}`)
+        .setImage(user.user.avatarURL)
+        .setColor(user.colorRole.color || 'BLUE')
+        .addField('Full username:', `${user.user.username}#${user.user.discriminator}`)
+        .addField('Joined Discord:', user.user.createdAt);
+}
+
+function getColour(args) {
+    let colour = [];
+    //User supplied rgb values
+    if (!isNaN(args[0]) && !isNaN(args[1]) && !isNaN(args[2])) {
+        colour[0] = clamp(args[0], 1, 255);
+        colour[1] = clamp(args[1], 1, 255);
+        colour[2] = clamp(args[2], 1, 255);
+    }
+    //Otherwise generate a random number
+    else {
+        colour[0] = getRandomInt(1,256);
+        colour[1] = getRandomInt(1,256);
+        colour[2] = getRandomInt(1,256);
+    }
+    return colour;
+}
+
+//Helper functions
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function clamp(n, min, max) {
+    return Math.min(Math.max(n, min), max);
 }
