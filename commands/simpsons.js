d25f0c28289be133f4cbfc30bd6121e8bb90c0d7
yung lean <33080132+Michael23B@users.noreply.github.com>
2018-06-22 17:12:07 +1000

    Added more info to simpsons command vote

--- commands/simpsons.js
+++ commands/simpsons.js
@@ -7,7 +7,7 @@ const filter = (reaction) => reaction.emoji.name === '1⃣'
     || reaction.emoji.name === '4⃣';
 
 module.exports.run = async(client, message, args) => {
-    let loadingMsg = await message.channel.send('Searching for Simpsons frame...').catch(console.error);
+    let loadingMsg = await message.channel.send('Searching Simpsons database...').catch(console.error);
 
     let query = Array.prototype.join.call(args.slice(0), " ");
     let url = query ? `https://frinkiac.com/api/search?q=${query}` : 'https://frinkiac.com/api/random';
@@ -21,42 +21,44 @@ module.exports.run = async(client, message, args) => {
         await message.channel.send(`Result for random search`, {files: [imgUrl]});
         return;
     }
-    let embed = getResultsSampleEmbed(resultsObj, query);
+
+    let info = await generateDetails(resultsObj);
+
+    let embed = getResultsSampleEmbed(info, query);
 
     let msg = await message.channel.send(embed);
+    loadingMsg.delete();
 
     await msg.react('1⃣').catch(console.error);
     await msg.react('2⃣').catch(console.error);
     await msg.react('3⃣').catch(console.error);
     await msg.react('4⃣').catch(console.error);
 
-    let winningIndex = await msg.awaitReactions(filter, { time: 10000 })
+    let winningIndex = await msg.awaitReactions(filter, { time: 7500 })
         .then(collected => selectWinningEmoji(collected))
         .catch(console.error);
 
-
     let imgUrl = `https://frinkiac.com/img/${resultsObj[winningIndex].Episode}/${resultsObj[winningIndex].Timestamp}.jpg`;
 
-    //message.channel.send(`Result for "${query}" - frame number ${winningIndex}`, {files: [imgUrl]});
     message.channel.send(await getImageEmbed(imgUrl, query, winningIndex));
     msg.delete();
 };
 module.exports.aliases = ['simpsons', 'simpson'];
 
-function getResultsSampleEmbed(result, query) {
+function getResultsSampleEmbed(info, query) {
     return new Discord.RichEmbed()
         .setTitle(`Results for "${query}"`)
         .setColor('GOLD')
-        .addField(`1.`, `${result[0].Episode}`)
-        .addField(`2.`, `${result[1].Episode}`)
-        .addField(`3.`, `${result[2].Episode}`)
-        .addField(`4.`, `${result[3].Episode}`)
+        .addField(`1.`, `**Title**: ${info[0].Episode.Title}\n**Subtitle**: ${info[0].Subtitles[0].Content}\n**Frame**: ${info[0].Frame.Timestamp}`)
+        .addField(`2.`, `**Title**: ${info[1].Episode.Title}\n**Subtitle**: ${info[1].Subtitles[0].Content}\n**Frame**: ${info[1].Frame.Timestamp}`)
+        .addField(`3.`, `**Title**: ${info[2].Episode.Title}\n**Subtitle**: ${info[2].Subtitles[0].Content}\n**Frame**: ${info[2].Frame.Timestamp}`)
+        .addField(`4.`, `**Title**: ${info[3].Episode.Title}\n**Subtitle**: ${info[3].Subtitles[0].Content}\n**Frame**: ${info[3].Frame.Timestamp}`)
         .setFooter('React to select image.');
 }
 
 function getImageEmbed(imgUrl, query, winningIndex) {
     return new Discord.RichEmbed()
-        .setTitle(`Result for "${query}" - Frame ${winningIndex}`)
+        .setTitle(`Result for "${query}" - Frame ${winningIndex + 1}`)
         .setColor('GOLD')
         .setImage(imgUrl)
         .setFooter('React to go to the next or previous frame.');
@@ -85,3 +87,23 @@ function selectWinningEmoji(collected) {
             return 3;
     }
 }
+
+async function generateDetails(results) {
+    let frameData = [];
+
+    for (let i = 0; i < 4; ++i) {
+        frameData[i] = await getFrameInfo(results[i].Episode, results[i].Timestamp);
+    }
+
+    return frameData;
+}
+
+async function getFrameInfo(episode, frame) {
+    let url = `https://frinkiac.com/api/caption?e=${episode}&t=${frame}`;
+
+    return await superagent.get(url)
+        .then(res => res.body)
+        .catch(console.error);
+}
+
+//TODO: clean up the urls everywhere and replace with variables
